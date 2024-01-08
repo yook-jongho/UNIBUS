b43052be0ac609dbcdb3aadaa62ed7e42fd6d2ce
육종호 <dbrwhdgh1203@naver.com>
2024-01-08 19:59:30 +0900

    [feat]: 교통약자 유형 선택 화면 개발

---
+++ client/client.js
@@ -0,0 +1,29 @@
+document.getElementById("next").addEventListener("click", function () {
+    var radioButtons = document.getElementsByName("select");
+    var isChecked = null;
+
+    // 라디오 버튼 중 하나가 선택되었는지 확인
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            isChecked = radioButtons[i].value;
+            break;
+        }
+    }
+
+    // 라디오 버튼이 선택되지 않았을 경우 경고 표시
+    if (isChecked === null) {
+        alert("라디오 버튼을 선택해주세요.");
+    } else if (isChecked === "passenger") {
+        const move = "../client/client.html";
+        window.location.href = move;
+    } else if (isChecked === "driver") {
+        const move = "../driver/driver.html";
+        window.location.href = move;
+        // console.log("기사 페이지롱");
+    }
+});
+
+document.getElementById("prev").addEventListener("click", function () {
+    const move = "../select/select.html";
+    window.location.href = move;
+});
