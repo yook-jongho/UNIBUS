07804af7c576774fcfd1afddf48cd476adc54f18
육종호 <dbrwhdgh1203@naver.com>
2024-01-28 01:44:26 +0900

    feat: footer 추가

--- pages/roleSelect/index.js
+++ pages/roleSelect/index.js
@@ -1,10 +1,12 @@
 import { stepContainer } from "./stepContainer.js";
+import { footer } from "../../components/footer.js";
 
-const app = document.getElementById("app");
-const text = {
-    one: "버스 기사",
-    two: "버스 탑승객",
-};
-app.insertAdjacentHTML("afterbegin", stepContainer(text));
-// app.insertAdjacentHTML("afterend", button);
-// app.insertAdjacentHTML("afterend", logo);
+(() => {
+    const app = document.getElementById("app");
+    const text = {
+        one: "버스 기사",
+        two: "버스 탑승객",
+    };
+    app.insertAdjacentHTML("afterbegin", stepContainer(text));
+    app.insertAdjacentHTML("afterend", footer("다음"));
+})();
